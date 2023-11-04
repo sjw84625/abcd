a0f005ff98ccdb940620636e22869e89aeba8c9e
sjw84625 <149846604+sjw84625@users.noreply.github.com>
2023-11-04 10:37:15 +0900

    Update me.py

--- me.py
+++ me.py
@@ -4,5 +4,10 @@ import numpy as np
 
 st.title('신정우')
 
-st.write("Hello world")
-st.write(my_ddd)
+st.write('데이터프레임 튜토리얼')
+dataframe=pd.DataFrame({
+  'first column':[1,2,3,4]
+  'second column':[10,20,30,40]
+})
+st.dataframe(datareame,use_container_width=False)
+st.table(dataframe)
