472295486ac12d668b86e213b8b5f212c5c3edd4
sjw84625 <149846604+sjw84625@users.noreply.github.com>
2023-11-04 10:40:28 +0900

    Update me.py

--- me.py
+++ me.py
@@ -6,7 +6,7 @@ st.title('신정우')
 
 st.write('데이터프레임 튜토리얼')
 dataframe=pd.DataFrame({
-  'first column':[1,2,3,4]
+  'first column':[1,2,3,4],
   'second column':[10,20,30,40],
 })
 st.dataframe(datareame,use_container_width=False)
