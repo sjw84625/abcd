93d63cd0247780127327b2fefe5f5fadec4d0256
sjw84625 <149846604+sjw84625@users.noreply.github.com>
2023-11-11 10:50:26 +0900

    Update me.py

--- me.py
+++ me.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
 
 st.title("신정우's profile")
 
@@ -14,6 +13,3 @@ st.table(t.info())
 st.table(t.head())
 
 st.table(t.tail())
-
-table=t.pivot_table(index=['Sex'],columns=['Pclass'],aggfunc='size')
-sns.heatmap(table,annot=True,fmt='d',cmap='rainbow')
