9a9606470de7e906278ce5012c0426c7e9804533
sjw84625 <149846604+sjw84625@users.noreply.github.com>
2023-11-11 09:43:55 +0900

    Update me.py

--- me.py
+++ me.py
@@ -20,3 +20,11 @@ col1, col2, col3 = st.columns(3)
 col1.metric(label="달러USD", value="1,228 원", delta="-12.00 원")
 col2.metric(label="일본JPY(100엔)", value="958.63 원", delta="-7.44 원")
 col3.metric(label="유럽연합EUR", value="1,335.82 원", delta="11.44 원")
+
+tab1,tab2=st.tabs(["Cat","Dog"])
+with tab1:
+  st.header("A cat")
+  st.image("https://static.streamlit.io/examples/cat.jpg",width=200)
+with tab2:
+  st.header("A dog")
+  st.image("https://static.streamlit.io/examples/dog.jpg",width=200)
