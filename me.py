4d57758ba6840d951d5737399f99189af6195731
sjw84625 <149846604+sjw84625@users.noreply.github.com>
2023-11-18 09:24:04 +0900

    Update me.py

--- me.py
+++ me.py
@@ -4,15 +4,16 @@ import numpy as np
 
 st.title("신정우's profile")
 
-
-st.metric(label="나이",value="중1",delta="14")
-
-path='train (1).csv'
-t=pd.read_csv(path)
-t
-
-st.table(t.head())
-
-st.table(t.tail())
-
-st.table(t.describe())
+tab1,tab2=st.tabs(["About Me","Projects"])
+with tab1:
+  st.metric(label="나이",value="중1",delta="14")
+with tab2:
+  path='train (1).csv'
+  t=pd.read_csv(path)
+  t
+  
+  st.table(t.head())
+  
+  st.table(t.tail())
+  
+  st.table(t.describe())
