31de70d3bea4b818e9f4aa533839b1c3edeeadf5
sjw84625 <149846604+sjw84625@users.noreply.github.com>
2023-11-18 09:26:24 +0900

    Update me.py

--- me.py
+++ me.py
@@ -8,6 +8,7 @@ tab1,tab2=st.tabs(["About Me","Projects"])
 with tab1:
   st.metric(label="나이",value="중1",delta="14")
 with tab2:
+  st.header("titanic data analysis")
   path='train (1).csv'
   t=pd.read_csv(path)
   t
