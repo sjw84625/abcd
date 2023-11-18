5825786ff044bb85858026c43781a32a703387ab
sjw84625 <149846604+sjw84625@users.noreply.github.com>
2023-11-18 09:53:23 +0900

    Update me.py

--- me.py
+++ me.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title("신정우's profile")
 
@@ -33,4 +35,4 @@ with tab2:
   st.header("생존 여성과 남성의 비율")
   st.table(t.groupby(['Sex','Survived'])['PassengerId'].count())
 
-  seaborn.barplot(x='Sex',y='Survived',data=t)
+  #seaborn.barplot(x='Sex',y='Survived',data=t)
