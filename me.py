dbdc23eb8ea9ba80866de551c00930a4cac15839
sjw84625 <149846604+sjw84625@users.noreply.github.com>
2023-11-18 09:42:47 +0900

    Update me.py

--- me.py
+++ me.py
@@ -22,6 +22,7 @@ with tab2:
   st.table(t.isna().sum())
 
   st.table(t.groupby('Embarked').count())
-
+  t['Age'].fillna(t['Age'].mean(),inplace=True)
+  t['Embarked'].unique()
   t['Embarked'].fillna('S',inplace=True)
   st.table(t.isna().sum())
